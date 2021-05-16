58dad20d2d84f431dd9fb10b58410c8af5de2217
Hookypoo <forde.hugh@yahoo.com>
2021-05-15 20:48:31 -0400

    add form component with Modal feature

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from "react-native";
 import { globalStyles } from "../styles/Global";
 import Card from "../shared/Card";
+import { MaterialIcons } from "@expo/vector-icons";
+import ReviewForm from "./ReviewForm";
 
 export default function Home({navigation}) {
+    const[modalOpen, setModalOpen]=useState("false");
 
     const [reviews, setReviews] = useState([
         { title: "Zelda Breath of Fresh Air", rating: 5, body: "loren ipsum", key:"1" },
@@ -13,6 +16,25 @@ export default function Home({navigation}) {
 
     return (
         <View style={globalStyles.container}>
+            <Modal visible={modalOpen} animationType="slide">
+                <View style={styles.modalContent}>
+                <MaterialIcons 
+                    name="close"
+                    size={24}
+                    style={{...styles.modalToggle, ...styles.modalClose}}
+                    onPress={() => setModalOpen(false)}
+                />
+                    <ReviewForm />
+                </View>
+            </Modal>
+
+            <MaterialIcons 
+            name="add"
+            size={24}
+            style={styles.modalToggle}
+            onPress={() => setModalOpen(true)}
+            />
+
             <FlatList 
                 data={reviews}
                 renderItem={({ item }) => (
@@ -26,6 +48,24 @@ export default function Home({navigation}) {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: "#f2f2f2",
+        paddingTop: 10,
+        borderRadius: 10,
+        alignSelf: "center",
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0,
+    },
+    modalContent: {
+        flex: 1,
+    }
+})
 
 
 
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
-import { globalStyles } from "../styles/Global";
+import { globalStyles, images } from "../styles/Global";
 import Card from "../shared/Card";
 
-export default function ReviewDetails({ navigation }) {    
+
+
+export default function ReviewDetails({ navigation }) {  
+    const rating = navigation.getParam("rating");  
 
        return (
         
@@ -13,7 +16,7 @@ export default function ReviewDetails({ navigation }) {
                 <Text>{ navigation.getParam("body") }</Text>
                 <View style={styles.rating}>
                     <Text>GameZone rating: </Text> 
-                    <Image source={require("../assets/rating-1.png")} />               
+                <Image source={images.ratings[rating]} />  
                 </View> 
                 </Card>          
             </View>
@@ -23,9 +26,14 @@ export default function ReviewDetails({ navigation }) {
 
 const styles = StyleSheet.create({
     rating: {
-
+        flexDirection: "row",
+        justifyContent: "center",
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: "#eee",
     }
-})
+});
 
 
 
